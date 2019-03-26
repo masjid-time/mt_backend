ba7bb390904db7a9a86dc4f8cf65ddc733cee33f
Ansuman <ansuman5555@gmail.com>
2019-03-26 12:11:33 +0530

    Included prod url for CORS

--- mtapp/__init__.py
+++ mtapp/__init__.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from flask_restful import Api
 
 app = Flask(__name__)
-cors = CORS(app, origins=["http://localhost:3000/*"])
+cors = CORS(app, origins=['http://localhost:3000/*', 'https://prayertiming.herokuapp.com/*'])
 api = Api(app)
 
 from mtapp import routes
