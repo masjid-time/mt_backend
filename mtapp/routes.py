7db30f09006f358250a7123b29d816f56b772006
Ansuman <ansuman5555@gmail.com>
2019-04-09 03:25:30 +0530

    Added api endpoint for detail page

--- mtapp/routes.py
+++ mtapp/routes.py
@@ -2,7 +2,7 @@ from flask import jsonify
 from flask_restful import Resource
 
 from mtapp import api
-from mtapp.data import mosque_list
+from mtapp.data import mosque_list, mosque_detail
 
 
 class Mosques(Resource):
@@ -10,4 +10,16 @@ class Mosques(Resource):
         return jsonify(mosque_list)
 
 
+class MosqueDetail(Resource):
+    def get(self, mosque_id):
+        resp = {'found': False}
+        for mosque in mosque_detail:
+            if mosque['id'] == mosque_id:
+                resp['found'] = True
+                resp.update(mosque)
+                break
+        return jsonify(resp)
+
+
 api.add_resource(Mosques, '/api/v1/mosques')
+api.add_resource(MosqueDetail, '/api/v1/mosquedetail/<int:mosque_id>')
