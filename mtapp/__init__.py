d579601d7b9a90907cd8c8927ed3007035fa0868
Ansuman Ghosh <ansuman5555@gmail.com>
2019-05-27 14:06:24 +0530

    Updated CORS

--- mtapp/__init__.py
+++ mtapp/__init__.py
@@ -6,7 +6,7 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-cors = CORS(app, origins=['http://localhost:3000/*', 'https://prayertiming.herokuapp.com/*', 'http://www.masjidtime.com/*'])
+cors = CORS(app, origins=['http://localhost:3000/*', 'https://prayertiming.herokuapp.com/*', 'https://masjidtime.herokuapp.com/*', 'https://www.masjidtime.com/*'])
 api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
